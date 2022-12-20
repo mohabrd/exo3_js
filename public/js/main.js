7a66d4a27f66e18800cb4afec15e2c31485cc11f
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-20 10:28:04 +0100

    exo math js fini

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+let nombre = 8.56;
+
+console.log(Math.round(nombre));
+
+console.log(Math.ceil(nombre));
+
+console.log(Math.floor(nombre));
+
+console.log(Math.trunc(nombre));
+
+console.log(Math.random());
+console.log(Math.random()*100);
+console.log(Math.round(Math.random()*100));
+
+console.log(Math.pow(8, 2));
+console.log(Math.sqrt(9));
+console.log(Math.abs(-1));
+console.log(Math.max(-2, 1000, 8, 57));
+console.log(Math.min(-2, 1000, 8, 57));
